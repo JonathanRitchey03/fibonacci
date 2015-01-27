2f07283ca747d489d0a6a1d7dec2ed953444d4f5
Jonathan Ritchey <jonathan@kurbo.com>
2015-01-27 08:40:01 -0800

    Add support for lookup to 999 & implement algorithm

--- src/lookup.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by kurbo on 1/25/15.
- */
-public class lookup {
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
-    }
-}
--- src/org/fibonacci/Main.java
+++ src/org/fibonacci/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        for ( int i = 0; i < 200; i++ )
-        System.out.println("fib("+i+")="+Lookup.fibonacci(i));
+        for ( int i = 0; i < 10; i++ ) {
+            System.out.println("fib(" + i + ")=" + Lookup.fibonacci(i));
+        }
+        System.out.println("fib(32000)="+Lookup.fibonacci(32000));
     }
 }
